5eb11277b1fe276151f78a3415805aed45c46ee5
Ilana Bonder <ilanabonder@gmail.com>
2018-01-30 14:51:45 -0500

    laa

--- week2/server.js
+++ week2/server.js
@@ -5,6 +5,20 @@ app.get('/', function (req,res) {
   res.send('Hello World!')
 });
 
+app.get('/formpost', function(req, res) {
+  res.send("You submitted "+ req.query.textfield);
+  thesubmissions.push(req.query.textfield);
+  //req.query.checkboxname
+});
+
+app.get('/display', function(req, res) {
+  var htmlout = "<html><body>";
+  for (var i =0; i < thesubmissions.length; i++) {
+    htmlout = htmlout + thesubmissions[i] + "<br>";
+  }
+  var htmlout = htmlout "</body></html>";
+})
+
 app.listen(300, function () {
   console.log('Example app listening on port 3000!')
 })
