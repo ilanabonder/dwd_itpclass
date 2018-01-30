adc39ca1de6da2ba30abb5a33f24fda6825cdc38
Ilana Bonder <ilanabonder@gmail.com>
2018-01-30 15:54:38 -0500

    copying shawns code

--- week2/server.js
+++ week2/server.js
@@ -1,24 +1,41 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req,res) {
+app.use(express.static('public'));
+
+var count = 0;
+
+var thesubmissions = [];
+
+app.get('/formpost', function(req, res) {
+  //res.send("You submitted " + req.query.textfield);
+  thesubmissions.push(req.query.textfield);
+  res.redirect('/display');
+});
+
+app.get('/display', function(req, res) {
+  var htmlout = "<html><body>";
+  for (var i = 0; i < thesubmissions.length; i++) {
+    htmlout = htmlout + thesubmissions[i] + "<br>";
+  }
+  var htmlout = htmlout + "</body></html>";
+  res.send(htmlout);
+});
+
+app.get('/count', function(req, res) {
+  count++;
+  res.send("<html><body><h1>"+count+"</h1></body></html>");
+});
+
+app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.get('/formpost', function(req, res) {
-  res.send("You submitted "+ req.query.textfield);
-  thesubmissions.push(req.query.textfield);
-  //req.query.checkboxname
-});
+app.get('/somethingelse', function(req, res) {
+  res.send("Goodbye");
 
-// app.get('/display', function(req, res) {
-//   var htmlout = "<html><body>";
-//   for (var i =0; i < thesubmissions.length; i++) {
-//     htmlout = htmlout + thesubmissions[i] + "<br>";
-//   }
-//   var htmlout = htmlout "</body></html>";
-// })
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
